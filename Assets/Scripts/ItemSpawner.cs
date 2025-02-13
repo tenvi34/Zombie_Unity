4d1ee7ad172323aa6619f2289dd161f69bd26ecb
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2025-02-13 17:02:53 +0900

    포스트 프로세스 적용 및 게임 완성

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -2,7 +2,8 @@
 using UnityEngine.AI; // 내비메쉬 관련 코드
 
 // 주기적으로 아이템을 플레이어 근처에 생성하는 스크립트
-public class ItemSpawner : MonoBehaviour {
+public class ItemSpawner : MonoBehaviour
+{
     public GameObject[] items; // 생성할 아이템들
     public Transform playerTransform; // 플레이어의 트랜스폼
 
@@ -14,14 +15,16 @@ public class ItemSpawner : MonoBehaviour {
 
     private float lastSpawnTime; // 마지막 생성 시점
 
-    private void Start() {
+    private void Start()
+    {
         // 생성 간격과 마지막 생성 시점 초기화
         timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
         lastSpawnTime = 0;
     }
 
     // 주기적으로 아이템 생성 처리 실행
-    private void Update() {
+    private void Update()
+    {
         // 현재 시점이 마지막 생성 시점에서 생성 주기 이상 지남
         // && 플레이어 캐릭터가 존재함
         if (Time.time >= lastSpawnTime + timeBetSpawn && playerTransform != null)
@@ -36,7 +39,8 @@ public class ItemSpawner : MonoBehaviour {
     }
 
     // 실제 아이템 생성 처리
-    private void Spawn() {
+    private void Spawn()
+    {
         // 플레이어 근처에서 내비메시 위의 랜덤 위치 가져오기
         Vector3 spawnPosition =
             GetRandomPointOnNavMesh(playerTransform.position, maxDistance);
@@ -53,7 +57,8 @@ public class ItemSpawner : MonoBehaviour {
 
     // 내비메시 위의 랜덤한 위치를 반환하는 메서드
     // center를 중심으로 distance 반경 안에서 랜덤한 위치를 찾는다
-    private Vector3 GetRandomPointOnNavMesh(Vector3 center, float distance) {
+    private Vector3 GetRandomPointOnNavMesh(Vector3 center, float distance)
+    {
         // center를 중심으로 반지름이 maxDistance인 구 안에서의 랜덤한 위치 하나를 저장
         // Random.insideUnitSphere는 반지름이 1인 구 안에서의 랜덤한 한 점을 반환하는 프로퍼티
         Vector3 randomPos = Random.insideUnitSphere * distance + center;
